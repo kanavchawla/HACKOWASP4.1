471431d5884b57c21b90d17023be48c5c2e2d272
kanavchawla <98641838+kanavchawla@users.noreply.github.com>
2022-05-01 04:26:07 +0530

    Add files via upload

---
+++ Hacko4.1/calcVendor.js
@@ -0,0 +1,11 @@
+const userInput = document.getElementById('input-number');
+const addBtn = document.getElementById('btn-add');
+
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
+}
